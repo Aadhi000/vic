84f0779be2007153ef2588b318b2e3707f7d6f10
Deepakkrishna35 <89923452+Deepakkrishna35@users.noreply.github.com>
2023-04-28 16:48:47 +0530

    Update info.py

--- info.py
+++ info.py
@@ -33,7 +33,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FILO:FILO@cluster0.rvwnqye.mongodb.net/?retryWrites=true&w=majority")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://FILO:FILO@cluster0.xzytvve.mongodb.net/?retryWrites=true&w=majority")
 DATABASE_NAME = environ.get('DATABASE_NAME', "FILO")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Aaxx_fikes')
 
