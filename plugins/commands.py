5b03b60f61c18241bad030cd9c7d55140ef1e746
Deepakkrishna35 <89923452+Deepakkrishna35@users.noreply.github.com>
2023-04-28 17:15:02 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -241,7 +241,7 @@ async def start(client, message):
         chat_id=message.from_user.id,
         file_id=file_id,
         caption=f_caption,
-        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('sᴜʙsᴄʀɪʙᴇ', url='youtube.com/OpusTechz') ] ] ),
+        reply_markup=InlineKeyboardMarkup( [ [ InlineKeyboardButton('Cʜᴀɴɴᴇʟ Lɪɴᴋs', url='https://t.me/CINEMAGRAM_LINKS') ] ] ),
         protect_content=True if pre == 'filep' else False,
         )
                     
