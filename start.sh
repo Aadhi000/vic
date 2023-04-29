b103f613dd0008a83eb704ea5c6fdb3716adaf3c
ααdhí <87893573+Aadhi000@users.noreply.github.com>
2023-04-29 09:57:09 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Albinmanoj1/Auto.git /Auto
+  git clone https://github.com/Aadhi000/vic.git /vic
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Auto
